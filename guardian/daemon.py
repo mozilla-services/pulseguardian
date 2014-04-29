03f78b469f8c448d4a625c5126006c04c9317331
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-04-29 16:10:47 -0700

    More examples of using the API, error reporting

--- guardian/daemon.py
+++ guardian/daemon.py
@@ -10,15 +10,26 @@ API_BASE_URL = 'http://{}:{}/api'.format(DEFAULT_RABBIT_HOST, DEFAULT_RABBIT_API
 
 class PulseManagementAPI(object):
     def _api_request(self, path):
-        return requests.get('{}/{}'.format(API_BASE_URL, path), auth=(DEFAULT_RABBIT_USER, DEFAULT_RABBIT_PASSWORD)).json()
+        response = requests.get('{}/{}'.format(API_BASE_URL, path), auth=(DEFAULT_RABBIT_USER, DEFAULT_RABBIT_PASSWORD))
+        try:
+            return response.json()
+        except ValueError:
+            raise ValueError("Error when calling the API with path '{}'. Received : {}".format(path, response.content))
+
+    def queues(self, vhost=None):
+        if vhost:
+            return self._api_request('queues/{}'.format(vhost))
+        else:
+            return self._api_request('queues')
     
-    def queues(self):
-        return self._api_request('queues')
-    
+    def queue_details(self, vhost, queue):
+        return self._api_request('queues/{}/{}'.format(vhost, queue))
+
+
     def vhosts(self):
         return self._api_request('vhosts')
 
 if __name__ == '__main__':
     api = PulseManagementAPI()
-    print api.queues()
-    print api.vhosts()
+    for queue in api.queues(vhost=DEFAULT_RABBIT_VHOST):
+        print "Queue '{}' : {} messages".format(queue['name'], queue['messages_ready'])
