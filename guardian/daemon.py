05743bce8e9a9e2fe50786e73b30a83e9a8418d7
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-04-29 15:53:02 -0700

    Cleaner API communication

--- guardian/daemon.py
+++ guardian/daemon.py
@@ -6,9 +6,19 @@ DEFAULT_RABBIT_API_PORT = 15672
 DEFAULT_RABBIT_VHOST = '/'
 DEFAULT_RABBIT_USER = 'guest'
 DEFAULT_RABBIT_PASSWORD = 'guest'
+API_BASE_URL = 'http://{}:{}/api'.format(DEFAULT_RABBIT_HOST, DEFAULT_RABBIT_API_PORT)
 
+class PulseManagementAPI(object):
+    def _api_request(self, path):
+        return requests.get('{}/{}'.format(API_BASE_URL, path), auth=(DEFAULT_RABBIT_USER, DEFAULT_RABBIT_PASSWORD)).json()
+    
+    def queues(self):
+        return self._api_request('queues')
+    
+    def vhosts(self):
+        return self._api_request('vhosts')
 
 if __name__ == '__main__':
-    api_url = 'http://{}:{}/api'.format(DEFAULT_RABBIT_HOST, DEFAULT_RABBIT_API_PORT)
-    print api_url
-    print requests.get('{}/vhosts'.format(api_url), auth=(DEFAULT_RABBIT_USER, DEFAULT_RABBIT_PASSWORD)).text
+    api = PulseManagementAPI()
+    print api.queues()
+    print api.vhosts()
