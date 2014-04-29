cee566b960433399a9852dd43e9d6d457849f2d3
Ahmed Kachkach <ahmed.kachkach@gmail.com>
2014-04-29 16:21:55 -0700

    Configurable API class, preparing for a Guardian class

--- guardian/daemon.py
+++ guardian/daemon.py
@@ -2,15 +2,28 @@ import requests
 
 DEFAULT_RABBIT_HOST = 'localhost'
 DEFAULT_RABBIT_PORT = 5672
-DEFAULT_RABBIT_API_PORT = 15672
+DEFAULT_RABBIT_MANAGEMENT_PORT = 15672
 DEFAULT_RABBIT_VHOST = '/'
 DEFAULT_RABBIT_USER = 'guest'
 DEFAULT_RABBIT_PASSWORD = 'guest'
-API_BASE_URL = 'http://{}:{}/api'.format(DEFAULT_RABBIT_HOST, DEFAULT_RABBIT_API_PORT)
+
+WARN_QUEUE_SIZE = 2
+ARCHIVE_QUEUE_SIZE = 15
+DEL_QUEUE_SIZE = 20
 
 class PulseManagementAPI(object):
+    def __init__(self, host=DEFAULT_RABBIT_HOST, management_port=DEFAULT_RABBIT_MANAGEMENT_PORT, vhost=DEFAULT_RABBIT_VHOST,
+        user=DEFAULT_RABBIT_USER, password=DEFAULT_RABBIT_PASSWORD):
+
+        self.host = host
+        self.management_port = management_port
+        self.vhost = vhost
+        self.user = user
+        self.password = password
+    
     def _api_request(self, path):
-        response = requests.get('{}/{}'.format(API_BASE_URL, path), auth=(DEFAULT_RABBIT_USER, DEFAULT_RABBIT_PASSWORD))
+        response = requests.get('http://{}:{}/api/{}'.format(self.host, self.management_port, path),
+                                 auth=(self.user, self.password))
         try:
             return response.json()
         except ValueError:
@@ -28,6 +41,7 @@ class PulseManagementAPI(object):
 
     def vhosts(self):
         return self._api_request('vhosts')
+
 
 if __name__ == '__main__':
     api = PulseManagementAPI()
