9c50bc5f609e4618a049aead85deceba77a2d597
Ricardo Castro <mcc.ricardo@gmail.com>
2014-10-25 14:48:56 +0100

    Fixed wrong variable attribution.

--- pulseguardian/web.py
+++ pulseguardian/web.py
@@ -62,7 +62,7 @@ def load_fake_account(fake_account):
     # Check if user already exists in the database, creating it if not.
     g.user = User.query.filter(User.email == fake_account).first()
     if g.user is None:
-        g.useruser = User.new_user(email=fake_account)
+        g.user = User.new_user(email=fake_account)
 
 
 def requires_login(f):
