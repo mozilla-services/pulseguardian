e8daca01ebff435c23061247a55dc096ae2435ff
Ricardo Castro <mcc.ricardo@gmail.com>
2014-10-30 19:50:14 +0000

    Replacing log calls with logging (root logger).

--- pulseguardian/web.py
+++ pulseguardian/web.py
@@ -216,7 +216,7 @@ def delete_queue(queue_name):
         try:
             pulse_management.delete_queue(vhost='/', queue=queue.name)
         except PulseManagementException as e:
-            logger.warning("Couldn't delete the queue '{0}' on "
+            logging.warning("Couldn't delete the queue '{0}' on "
                                "rabbitmq: {1}".format(queue_name, e))
             return jsonify(ok=False)
         db_session.delete(queue)
@@ -229,17 +229,17 @@ def delete_queue(queue_name):
 @app.route('/pulse-user/<pulse_username>', methods=['DELETE'])
 @requires_login
 def delete_pulse_user(pulse_username):
-    logger.info('Request to delete Pulse user "{0}".'.format(pulse_username))
+    logging.info('Request to delete Pulse user "{0}".'.format(pulse_username))
     pulse_user = PulseUser.query.filter(PulseUser.username == pulse_username).first()
 
     if pulse_user and (g.user.admin or pulse_user.owner == g.user):
         try:
             pulse_management.delete_user(pulse_user.username)
         except PulseManagementException as e:
-            logger.warning("Couldn't delete user '{0}' on "
+            logging.warning("Couldn't delete user '{0}' on "
                                "rabbitmq: {1}".format(pulse_username, e))
             return jsonify(ok=False)
-        logger.info('Pulse user "{0}" deleted.'.format(pulse_username))
+        logging.info('Pulse user "{0}" deleted.'.format(pulse_username))
         db_session.delete(pulse_user)
         db_session.commit()
         return jsonify(ok=True)
@@ -280,7 +280,7 @@ def auth_handler():
     # Oops, something failed. Abort.
     error_msg = "Couldn't connect to the Persona verifier ({0})".format(
         config.persona_verifier)
-    logger.error(error_msg)
+    logging.error(error_msg)
     return jsonify(ok=False, message=error_msg)
 
 
@@ -381,7 +381,7 @@ def cli(args):
         dev_cert = '%s.crt' % DEV_CERT_BASE
         dev_cert_key = '%s.key' % DEV_CERT_BASE
         if not os.path.exists(dev_cert) or not os.path.exists(dev_cert_key):
-            app.logger.info('Creating dev certificate and key.')
+            logging.info('Creating dev certificate and key.')
             werkzeug.serving.make_ssl_devcert(DEV_CERT_BASE, host='localhost')
         ssl_context = (dev_cert, dev_cert_key)
 
