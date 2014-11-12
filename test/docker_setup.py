cc8e7b1f6ba3ddce6cf0489726e58ef4725b7235
Ricardo Castro <mcc.ricardo@gmail.com>
2014-11-12 22:39:19 +0000

    Placeholder for container setup and teardown.

---
+++ test/docker_setup.py
@@ -0,0 +1,11 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+from subprocess import call
+
+def setup_container():
+	pass
+
+def teardown_container():
+	pass
