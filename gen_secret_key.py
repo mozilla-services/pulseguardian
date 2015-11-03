b7867c3b67202d85aed5c1fcc28b515faedc7470
Mark Côté <mcote@alumni.uwaterloo.ca>
2015-11-03 00:43:21 -0500

    Add gen_secret_key.py.

---
+++ gen_secret_key.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+# Generates a 24-byte random base64-encoded string, which can be used as a
+# Flask secret key.
+import base64
+import os
+
+key = os.urandom(24)
+print base64.b64encode(key)
