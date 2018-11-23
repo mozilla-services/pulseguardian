bdea3917c9f5c2512b0d37ae4a099c4a446750a6
Mark Côté <mcote@mozilla.com>
2018-11-22 22:03:53 -0500

    Move deletable-object handlers to their own file.

--- pulseguardian/static/js/profile.js
+++ pulseguardian/static/js/profile.js
@@ -2,31 +2,10 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-// FIXME: There are a few functions related to "deletableObjects" (a server
-// resource that has a DELETE API and which, on the client side, uses a
-// confirmation dialog) which should be organized better.  See also
-// dialogs.html.
-
 $(document).ready(function() {
     // Auto-reload
     var autoReload = true;
     var reloadInterval = 8000;
-
-    function deletableObjectHandler(collectionClass, objectType) {
-        $('.' + objectType + 's .delete').click(function() {
-            var objectInstance = $(this).closest('.' + objectType);
-            var objectName = objectInstance.data(objectType + '-name');
-            var modal = $('.modal-delete-' + objectType);
-            modal.data(objectType + '-object', objectInstance);
-            modal.data('csrf-token',
-                       $('.' + collectionClass).data('csrf-token'));
-            modal.find('.' + objectType + '-name').text(objectName);
-            modal.modal();
-        });
-    }
-
-    deletableObjectHandler('queues', 'queue');
-    deletableObjectHandler('pulse-users', 'pulse-user');
 
     $('.pulse-users .edit').click(function() {
         var details = $($(this).closest('.pulse-user'))
@@ -52,42 +31,4 @@ $(document).ready(function() {
         autoReload = !autoReload;
         $(this).toggleClass('inactive');
     });
-
-    function deletableObject(objectType) {
-        function deleteObject(objectInstance, objectName, csrfToken) {
-            $.ajax({
-                url: '/' + objectType + '/' + objectName,
-                type: 'DELETE',
-                headers: {
-                    'X-CSRF-Token': csrfToken,
-                },
-                success: function(result) {
-                    if (!result.ok) {
-                        errorMessage("Couldn't delete " + objectType + " '" +
-                                     objectName + "'.");
-                        return;
-                    }
-
-                    $(objectInstance).slideUp(300);
-                },
-                error: function() {
-                    errorMessage("Couldn't delete " + objectType + " '" +
-                             objectName + "'.");
-                },
-                complete: function() {
-                    $('.modal-delete-' + objectType).modal('hide');
-                }
-            });
-        }
-
-        var modalClass = '.modal-delete-' + objectType;
-        $(modalClass + ' .delete-' + objectType + '-ok').click(function() {
-            deleteObject($(modalClass).data(objectType + '-object'),
-                         $(modalClass + ' .' + objectType + '-name').text(),
-                         $(modalClass).data('csrf-token'));
-        });
-    }
-
-    deletableObject('queue');
-    deletableObject('pulse-user');
 });
