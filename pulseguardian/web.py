bf3f07825fdfa40b76c342117d91120782e9d310
Dustin J. Mitchell <dustin@mozilla.com>
2019-11-14 17:28:14 +0000

    fix type of csrf token; fixes #252

--- pulseguardian/web.py
+++ pulseguardian/web.py
@@ -129,7 +129,7 @@ def csrf_exempt(view):
 
 def generate_csrf_token():
     if '_csrf_token' not in session:
-        session['_csrf_token'] = base64.b64encode(os.urandom(24))
+        session['_csrf_token'] = base64.b64encode(os.urandom(24)).decode('ascii')
     return session['_csrf_token']
 
 app.jinja_env.globals['csrf_token'] = generate_csrf_token
